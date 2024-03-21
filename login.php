4017c565d53c8114cad78fe5645c32b1429c9c78
DominikSubocz <dominiksubocz@gmail.com>
2024-03-21 21:01:11 +0000

    Styling Register/Login Pages
    Finished styling register and login page, I also changed style for the form according to the design image.

--- login.php
+++ login.php
@@ -37,77 +37,83 @@ Components::pageHeader("Login", ["style"], ["mobile-nav"]);
 
 ?>
 
-<h2>Log in to an existing account</h2>
-
-<form
-    method="POST"
-    action="<?php echo $_SERVER["PHP_SELF"];?>" 
-    class="form">
-
-        <label>Username</label>
-        <input
-            type="text"
-            name="username"
-            value="<?php
-
-            if($output && isset($_POST["loginSubmit"]) && isset($_POST["username"])){
-                echo Utils::escape($_POST["username"]);
-            }
-
-            ?>"
-        >
-
-        <label>Password</label>
-        <input type="password" name="password">
-
-        <input class="button" type="submit" name="loginSubmit" value="Log in">
-
-        <?php if ($output && isset($_POST["loginSubmit"])) { echo $output; } ?>
-</form>
-
-<hr>
-
-<h2> Register a new account</h2>
-
-<form
-    method="POST"
-    action="<?php echo $_SERVER["PHP_SELF"];?>" 
-    class="form">
-
-    <label for="username">Username</label>
-    <input
-        type="text"
-        name="username"
-        id="username"
-        value="<?php
-            if ($output && isset($_POST["registerSubmit"]) && isset($_POST["username"])) {
-                echo Utils::escape($_POST["username"]);
-            }
-        ?>"
-    >
-
-    <label for="email">Email address</label>
-    <input
-        type="email"
-        name="email"
-        id="email"
-        value="<?php
-            if ($output && isset($_POST["registerSubmit"]) && isset($_POST["email"])) {
-                echo Utils::escape($_POST["email"]);
-            }
-        ?>"
-    >
-
-    <label for="passwordOne">Password</label>
-    <input type="password" name="passwordOne" id="passwordOne">
-
-    <label for="passwordTwo">Password (retype)</label>
-    <input type="password" name="passwordTwo" id="passwordTwo">
-
-    <input class="button" type="submit" name="registerSubmit" value="Register account">
-
-    <?php if ($output && isset($_POST["registerSubmit"])) { echo $output; } ?>
-</form>
+<div class="authentication-container">
+    <div class="login-container">
+        <h2>Log in to an existing account</h2>
+    
+        <form
+            method="POST"
+            action="<?php echo $_SERVER["PHP_SELF"];?>"
+            class="form">
+    
+                <label>Username</label>
+                <input
+                    type="text"
+                    name="username"
+                    value="<?php
+    
+                    if($output && isset($_POST["loginSubmit"]) && isset($_POST["username"])){
+                        echo Utils::escape($_POST["username"]);
+                    }
+    
+                    ?>"
+                >
+    
+                <label>Password</label>
+                <input type="password" name="password">
+    
+                <input class="button" type="submit" name="loginSubmit" value="Log in">
+    
+                <?php if ($output && isset($_POST["loginSubmit"])) { echo $output; } ?>
+        </form>
+    </div>
+    
+    <hr>
+    
+    <div class="register-container">
+        <h2> Register a new account</h2>
+    
+        <form
+            method="POST"
+            action="<?php echo $_SERVER["PHP_SELF"];?>"
+            class="form">
+    
+            <label for="username">Username</label>
+            <input
+                type="text"
+                name="username"
+                id="username"
+                value="<?php
+                    if ($output && isset($_POST["registerSubmit"]) && isset($_POST["username"])) {
+                        echo Utils::escape($_POST["username"]);
+                    }
+                ?>"
+            >
+    
+            <label for="email">Email address</label>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                value="<?php
+                    if ($output && isset($_POST["registerSubmit"]) && isset($_POST["email"])) {
+                        echo Utils::escape($_POST["email"]);
+                    }
+                ?>"
+            >
+    
+            <label for="passwordOne">Password</label>
+            <input type="password" name="passwordOne" id="passwordOne">
+    
+            <label for="passwordTwo">Password (retype)</label>
+            <input type="password" name="passwordTwo" id="passwordTwo">
+    
+            <input class="button" type="submit" name="registerSubmit" value="Register account">
+    
+            <?php if ($output && isset($_POST["registerSubmit"])) { echo $output; } ?>
+        </form>
+    </div>
+</div>
 
 
 <?php
