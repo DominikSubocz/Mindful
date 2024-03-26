93a8429b2d2ded36770666b8933e5afc4aa585a4
Dominik Subocz <dominiksubocz@gmail.com>
2024-03-26 12:42:36 +0000

    Styling Index
    I am actually quite impressed that I managed to pull this off, but I guess that's problem with my confidence level.
    
    Anyway, I haven't finished adding bits and styling index so I'm going to do that next, but I managed to add and style the hero bit (I thinkt that's what it's called) and the steps bit, according to the design.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $location = "";
         action="<?php echo $_SERVER["PHP_SELF"];?>"
         class="form">
 
-        <div>
+        <div class="search-form-container">
             <input type="text" name="location" placeholder="Search articles" value="<?php echo $location;?>">
             <input class="button" type="submit" name="searchSubmit" value="Search">
         </div>
@@ -31,3 +31,34 @@ $location = "";
     </form>
 
 </div>
+
+<div class="index-steps-container">
+    <div class="step-card step-one">
+        <img src="images/isometric-woman-meditating-at-home.png" alt="Woman sitting meditating at home." class="step-card-img">
+        <h2>Step 1</h2>
+        <p>Find relevant articles. Type in
+            keywords or browse through 
+            categories to discover insights for
+        </p>
+    </div>
+    <div class="step-card step-two">
+        <img src="images/bendy-meeting-of-two-friends.png" alt="Two friends meeting, having a coffe." class="step-card-img">
+        <h2>Step 2</h2>
+        <p>Save your favourite articles. Keep
+            track of your progress and revisit
+            them whenever you need a boost.
+        </p>
+    </div>
+    <div class="step-card step-three">
+        <img src="images/isometric-man-watching-tv-at-home.png" alt="Man sitting on a couch watching TV." class="step-card-img">
+
+        <h2>Step 3</h2>
+        <p>Connect with the Mindful 
+            comminuty. Share your thoughts
+            and experiences with like-minded.
+        </p>
+    </div>
+
+</div>
+
+<br><br>
