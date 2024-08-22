8f984e06a09f56c46ed100062ea91c0fe35c4b1f
DominikSubocz <dominiksubocz@gmail.com>
2024-08-22 20:31:27 +0100

    Style Update

--- components/tag-row.php
+++ components/tag-row.php
@@ -1,1 +1,5 @@
-<button class="button-blank"><?php echo $tagName; ?><input name="<?php echo $tagName; ?>"  type="checkbox" id="myCheck" onclick="tickTag()"></button> 
+<div class="tagContainer">
+    <button class="button-blank"><?php echo $tagName; ?></button>
+    <input name="<?php echo $tagName; ?>"  class="tagBox" type="checkbox" id="myCheck" onclick="tickTag()">
+    <span class="checkmark"></span>
+</div>
