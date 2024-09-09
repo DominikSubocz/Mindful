70158ae1cdadc6e21502765690d134c1a85d10c4
DominikSubocz <dominiksubocz@gmail.com>
2024-09-09 13:14:56 +0100

    Images & Better Formatting
    I have added an option to add an image from the assets tab that also allows you to write text beside the just inserted image.
    
    Apart from that I have improved my code a little bit.

--- js/formatting.js
+++ js/formatting.js
@@ -15,13 +15,22 @@ function insertImg(img){
   
   const newImg = document.createElement("img");
   const imageDiv = document.createElement("div");
+  const imgSpan = document.createElement("span");
+  const belowSpan = document.createElement("span");
+
+  imgSpan.contentEditable = "true";
+  belowSpan.contentEditable = "true";
+
 
   imageDiv.className = "editorImageText"
   newImg.className = "postImg"
   newImg.src = img;
   imageDiv.appendChild(newImg);
 
+  imageDiv.appendChild(imgSpan);
   document.getElementById("editorTextArea").appendChild(imageDiv);
+  document.getElementById("editorTextArea").appendChild(belowSpan);
+
 }
 
 document.getElementById("close").addEventListener("click", hideAssets);
@@ -34,9 +43,14 @@ function format(type){
 
     if(sel.rangeCount){
         const newText = document.createElement("span");
+        let text = "";
         var range = sel.getRangeAt(0);
         var styledElement = window.getSelection().getRangeAt(0).endContainer.parentNode;
-        newText.appendChild(range.extractContents());
+        if((type != "fa fa-list") && (type != "fa fa-list-ol") && (type != "fa fa-quote-left")){
+          newText.appendChild(range.extractContents());
+        } else{
+          text = sel.toString();
+        }
 
         console.log(styledElement);
         if(type === "fa fa-bold"){
@@ -45,80 +59,62 @@ function format(type){
           } else {
             newText.style.fontWeight = "bold";
           }
-        } else {
-          if(type === "fa fa-italic"){
-            if(styledElement.style.fontStyle === "italic"){
-              newText.style.fontStyle = "normal";
-            } else {
-              newText.style.fontStyle = "italic";
-            }
+        } else if(type === "fa fa-italic"){
+          if(styledElement.style.fontStyle === "italic"){
+            newText.style.fontStyle = "normal";
           } else {
-            if(type === "fa fa-underline"){
-              if(styledElement.style.textDecoration === "underline"){
-                newText.style.textDecoration = "none";
-              } else {
-                newText.style.textDecoration = "underline";
-              }
-            } else {
-              if(type === "fa fa-strikethrough"){
-                if(styledElement.style.textDecoration === "line-through"){
-                  newText.style.textDecoration = "none";
-
-                } else {
-                newText.style.textDecoration = "line-through";
-              }
-              } else{
-                if(type === "fa fa-list"){
-                  let list = "<ul>\n"
-                  console.log(sel.toString());
-                  let text = sel.toString();
-                  const myArray = text.split("\n");
-                  console.log(myArray);
-                  if(myArray.length > 1){
-                    for(let i = 0; i < myArray.length; i++){
-                      list = list + "<li>" + myArray[i] + "</li>\n";
-                    }
-
-                    list = list + "\n</ul>";
-
-                    console.log(list);
-                    console.log(myArray.length);
-
-                    newText.innerHTML = list;
-                  }
-                } else {
-                  if(type === "fa fa-list-ol"){
-                    let list = "<ol>\n"
-                    console.log(sel.toString());
-                    let text = sel.toString();
-                    const myArray = text.split("\n");
-                    console.log(myArray);
-                    if(myArray.length > 1){
-                      for(let i = 0; i < myArray.length; i++){
-                        list = list + "<li>" + myArray[i] + "</li>\n";
-                      }
-  
-                      list = list + "\n</ol>";
-  
-                      console.log(list);
-                      console.log(myArray.length);
-  
-                      newText.innerHTML = list;
-                  }
-                } else {
-                  if(type === "fa fa-quote-left"){
-                    const fragment = range.cloneContents();
-                    newText.innerHTML = "\"" + sel.toString() + "\"";
-                  }
-                }
-              }
-            }
+            newText.style.fontStyle = "italic";
           }
+        } else if(type === "fa fa-underline"){
+          if(styledElement.style.textDecoration === "underline"){
+            newText.style.textDecoration = "none";
+          } else {
+            newText.style.textDecoration = "underline";
+          }
+        } else if(type === "fa fa-strikethrough"){
+          if(styledElement.style.textDecoration === "line-through"){
+            newText.style.textDecoration = "none";
+  
+          } else {
+          newText.style.textDecoration = "line-through";
         }
-        }
+      } else if(type === "fa fa-list"){
+        let list = "<ul>\n"
+        const myArray = text.split("\n");
+        if(myArray.length > 1){
+          for(let i = 0; i < myArray.length; i++){
+            list = list + "<li>" + myArray[i] + "</li>\n";
+          }
 
-      range.insertNode(newText);
-      sel.removeAllRanges();
+          list = list + "\n</ul>";
+
+          console.log(list);
+          console.log(myArray.length);
+          newText.innerHTML = list;
+        }
+      } else if(type === "fa fa-list-ol"){
+        let list = "<ol>\n"
+        const myArray = text.split("\n");
+        if(myArray.length > 1){
+          for(let i = 0; i < myArray.length; i++){
+            list = list + "<li>" + myArray[i] + "</li>\n";
+          }
+
+          list = list + "\n</ol>";
+
+          console.log(list);
+          console.log(myArray.length);
+          newText.innerHTML = list;
+      }
+    }  else if(type === "fa fa-quote-left"){
+      text = "\"" + text + "\"";
+      newText.innerHTML = text;
+    }
+
+    range.deleteContents();
+    range.insertNode(newText);
 
   }
+
+
 }
